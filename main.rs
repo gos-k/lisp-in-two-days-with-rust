7cfbd08ca202292be217a63f39f6fc1887f2a206
gos-k <mag4.elan@gmail.com>
2020-03-05 21:43:54 +0900

    Add call parse

--- main.rs
+++ main.rs
@@ -82,16 +82,66 @@ fn tokenise(source: &str) -> Vec<TokenKind> {
 pub enum Expr {
     Symbol(TokenKind, String),
     Number(TokenKind, i64),
-    If(TokenKind, TOkenKind, Box<Expr>, Box<Expr>, Box<Expr>, Token),
+    If(
+        TokenKind,
+        TokenKind,
+        Box<Expr>,
+        Box<Expr>,
+        Box<Expr>,
+        TokenKind,
+    ),
     Define(TokenKind, TokenKind, TokenKind, Box<Expr>, TokenKind),
     Call(TokenKind, TokenKind, Vec<Expr>, TokenKind),
 }
 
-fn parse_expr(&mut self) -> Expr {
-    if let Some(token) = self.0.next() {
+struct ParseState<I: Iterator<Item = TokenKind>>(std::iter::Peekable<I>);
+
+impl<I> ParseState<I>
+where
+    I: Iterator<Item = TokenKind>,
+{
+    fn parse_expr(&mut self) -> Expr {
+        if let Some(token_kind) = self.0.next() {
+            use TokenKind::*;
+            match token_kind {
+                LeftBracket => self.parse_form(token_kind),
+                RightBracket => panic!("unexpected token!"),
+                Number(n) => Expr::Number(token_kind, n),
+                Symbol(ref s) => {
+                    let sym = s.clone();
+                    Expr::Symbol(token_kind, sym)
+                }
+            }
+        } else {
+            panic!("invalid expression")
+        }
+    }
+
+    fn parse_form(&mut self, open: TokenKind) -> Expr {
+        use TokenKind::*;
+        match self.0.peek() {
+            Some(Symbol(ref sym)) => match &sym[..] {
+                _ => {
+                    let sym_tok = self.0.next().unwrap();
+                    let mut args = Vec::new();
+                    while let Some(token_kind) = self.0.peek() {
+                        if token_kind == &RightBracket {
+                            break;
+                        }
+                        args.push(self.parse_expr());
+                    }
+                    let close = self.0.next().unwrap();
+                    Expr::Call(open, sym_tok, args, close)
+                }
+            },
+            _ => panic!("invalid expression"),
+        }
     }
 }
 
 fn main() {
-    println!("{:?}", tokenise("(alfa bravo)"));
+    let tokens = tokenise("(alfa bravo)");
+    println!("{:?}", tokens);
+    let exprs = ParseState(tokens.into_iter().peekable()).parse_expr();
+    println!("{:?}", exprs);
 }
