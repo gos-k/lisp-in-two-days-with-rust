a5c230afa4c8910668f2eec836d37744b264895a
gos-k <mag4.elan@gmail.com>
2020-04-10 19:24:31 +0900

    Add print number

--- src/print.rs
+++ src/print.rs
@@ -1,8 +1,29 @@
 use super::eval::*;
 
+fn symbol_expression(result: Value) -> String {
+    use Value::*;
+
+    match result {
+        Number(number) => number.to_string(),
+        other => panic!("not impl {:?}", other),
+    }
+}
+
 pub fn print(result: EvalResult) {
     match result {
-        Ok(value) => println!(" ~> {:?}", value),
+        Ok(value) => println!(" ~> {}", symbol_expression(value)),
         Err(error) => println!(" !! {:?}", error),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_symbol_expression() {
+        use Value::*;
+
+        assert_eq!(symbol_expression(Number(0)), "0".to_string());
     }
 }
