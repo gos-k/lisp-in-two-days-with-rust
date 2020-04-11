3416edd07097f771a4b014173eeca73ae13c234c
gos-k <mag4.elan@gmail.com>
2020-04-11 18:16:56 +0900

    Add print parent

--- src/print.rs
+++ src/print.rs
@@ -1,11 +1,32 @@
 use super::eval::*;
 
-fn symbol_expression(result: Value) -> String {
+fn symbol_expression(result: Value, paren: bool) -> String {
     use Value::*;
 
     match result {
         Number(number) => number.to_string(),
         Symbol(symbol) => symbol.to_string(),
+        Parent(super::eval::Parent { lhs, rhs }) => {
+            let lhs = symbol_expression(*lhs, true);
+            match *rhs {
+                Nil => {
+                    if paren {
+                        format!("({})", lhs)
+                    } else {
+                        format!("{}", lhs)
+                    }
+                }
+                Parent(_) => {
+                    let rhs = symbol_expression(*rhs, false);
+                    if paren {
+                        format!("({} {})", lhs, rhs)
+                    } else {
+                        format!("{} {}", lhs, rhs)
+                    }
+                }
+                other => format!("({} . {})", lhs, symbol_expression(other, paren)),
+            }
+        }
         T => "t".to_string(),
         Nil => "nil".to_string(),
         other => panic!("not impl {:?}", other),
@@ -14,7 +35,7 @@ fn symbol_expression(result: Value) -> String {
 
 pub fn print(result: EvalResult) {
     match result {
-        Ok(value) => println!(" ~> {}", symbol_expression(value)),
+        Ok(value) => println!(" ~> {}", symbol_expression(value, true)),
         Err(error) => println!(" !! {:?}", error),
     }
 }
@@ -27,12 +48,24 @@ mod tests {
     fn test_symbol_expression() {
         use Value::*;
 
-        assert_eq!(symbol_expression(Number(0)), "0".to_string());
+        assert_eq!(symbol_expression(Number(0), true), "0".to_string());
         assert_eq!(
-            symbol_expression(Symbol("test".to_string())),
+            symbol_expression(Symbol("test".to_string()), true),
             "test".to_string()
         );
-        assert_eq!(symbol_expression(T), "t".to_string());
-        assert_eq!(symbol_expression(Nil), "nil".to_string());
+        assert_eq!(symbol_expression(cons(T, Nil), true), "(t)".to_string());
+        assert_eq!(
+            symbol_expression(cons(Number(0), Symbol("test".to_string())), true),
+            "(0 . test)".to_string()
+        );
+        assert_eq!(
+            symbol_expression(
+                cons(Number(0), cons(Symbol("test".to_string()), cons(T, Nil))),
+                true
+            ),
+            "(0 test t)".to_string()
+        );
+        assert_eq!(symbol_expression(T, true), "t".to_string());
+        assert_eq!(symbol_expression(Nil, true), "nil".to_string());
     }
 }
